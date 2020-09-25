7289b94d3197ef6d39b4c05076e9d5a5e53e7cf3
Fredys-MD <fredysmarquezd@unimagdalena.edu.co>
2020-09-25 11:34:56 -0500

    corrigiendo

--- Scripts/Queuing_theory_practice.R
+++ Scripts/Queuing_theory_practice.R
@@ -1,11 +1,4 @@
-# Práctica
-"""
 
-lambda: Tiempo de llegada 
-miu: Tiempo en servicio
-N: Población
-
-"""
 
 lambda <- 0.2
 miu   <- 0.25
@@ -17,22 +10,34 @@ N <- 10000
 tllegada <- rpois(N,lambda)
 tservicio <- rexp(N,miu)
 
-tllegadaCliente  <- cumsum(tllegada)
+tllegadaCliente  <- cumsum(tllegada) #momento en que llega cada cliente
 
 posicion = 1
 
 iniciServicio  <- rep(0,N); iniciServicio[posicion] <- tllegadaCliente[posicion]
 finServicio    <- rep(0,N); finServicio[posicion]   <- iniciServicio[posicion] + tservicio[posicion]
 
-vector_2_N <- 2:N
+vector_2_N <- 2:N # ya miramos el primer cliente
 
 for (i in vector_2_N) {
   iniciServicio[i] = max(tllegadaCliente[i],finServicio[i-1])
   finServicio[i] = iniciServicio[i] + tservicio[i]
 }
- 
-tEstadia <- finServicio - tllegadaCliente
-promMovil <- rep(2,N); promMovil <- tEstadia[posicion]
+
+# Tiempos en el sistema
+
+tEstadia <- finServicio - iniciServicio
+tEspera <- iniciServicio - tllegadaCliente
+tSistema <- finServicio - tllegadaCliente
+
+Cola <- data.frame("tllCliente" = tllegadaCliente,
+                   "tAtención" = tEstadia,
+                   "iniciServicio"=iniciServicio,
+                   "finServicio"=finServicio,
+                   "tSistema"=tSistema,
+                   "tEspera"= tEspera)
+
+promMovil <- rep(2,N); promMovil[posicion] <- tEstadia[posicion]
 
 
 for (i in vector_2_N) {
@@ -41,5 +46,5 @@ for (i in vector_2_N) {
 }
 
 # Gráfica
-pm = "Promedio movíl"
+pm = "tEstadia"
 plot(promMovil, type = "l", ylab = pm, main = pm, xlab="Número de clientes")
---
+++ Scripts/class.R
@@ -0,0 +1,13 @@
+
+library(lubridate)
+library(knitr)
+
+set.seed(12345678, "Mersenne-Twister")
+
+# Variables inicializadas
+
+lambda <- 3
+miu <- 4
+t <- 0
+ocupado <- FALSE
+cola <- 0
